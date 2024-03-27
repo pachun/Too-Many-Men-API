419e0e1e378ea7b84994c0ca565e16ae07a5b0ac
Nick Pachulski <nick@pachulski.me>
2024-03-27 02:24:57 -0400

    wip

--- app/commands/create_or_update_player_attendance.rb
+++ app/commands/create_or_update_player_attendance.rb
@@ -1,5 +1,17 @@
 class CreateOrUpdatePlayerAttendance
   def self.create_or_update(game_id:, player_id:, attending:)
+    new(game_id, player_id, attending).create_or_update
+  end
+
+  attr_reader :game_id, :player_id, :attending
+
+  def initialize(game_id, player_id, attending)
+    @game_id = game_id
+    @player_id = player_id
+    @attending = attending
+  end
+
+  def create_or_update
     PlayerAttendance.find_or_initialize_by(
       game_id: game_id,
       player_id: player_id,
